faa4a9774a4c1764db2380a8b69871853a9064b5
Ace <155284890+ILikeSaltedEgg@users.noreply.github.com>
2025-02-02 19:19:12 +0800

    Update Research1.php

--- System/Research1.php
+++ System/Research1.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Canteen Menu</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="/CanteenOnlineOrdering/Styles/styles.css">
 </head>
 <body>
 
