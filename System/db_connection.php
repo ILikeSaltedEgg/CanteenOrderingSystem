83590020492aee46fff10aaaee1537704e3d6a1b
Ace <155284890+ILikeSaltedEgg@users.noreply.github.com>
2026-02-07 19:53:33 +0800

    Add files via upload

--- System/db_connection.php
+++ System/db_connection.php
@@ -1,11 +1,13 @@
 <?php
-$host = '127.0.0.1';
-$username = 'root';
-$password = '';
-$dbname = 'au_canteen';
+$servername = "localhost"; // Change if necessary
+$username = "root"; // Your MySQL username
+$password = ""; // Your MySQL password
+$dbname = "foodieDB"; // Your database name
 
-$conn = new mysqli($host, $username, $password, $dbname);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
